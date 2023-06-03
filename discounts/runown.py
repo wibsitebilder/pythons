f897a91d32c929d4de8a09d91ee3a8c6d6f44405
free <118434460+wibsitebilder@users.noreply.github.com>
2023-06-03 19:28:29 +0300

    hi

--- discounts/runown.py
+++ discounts/runown.py
@@ -1,6 +1,7 @@
 print("Hi")
 numberofdiscount = float(input("Please Enter number of discount "))
 bdiscount = float(input("Please Enter a b of discount "))
+print("Wait.....")
 b = numberofdiscount / 100
 b2 = b * bdiscount
 mdis = numberofdiscount - b2
@@ -8,3 +9,8 @@ pdis = numberofdiscount + b2
 print(f"The number of discount is {b2}")
 print(f"The number m discount is {mdis}")
 print(f"The number p discount is {pdis}")
+print("Wait.....")
+maldis = numberofdiscount * b2
+divdis = numberofdiscount / b2
+print(f"The number mal discount is {maldis}")
+print(f"The number div discount is {divdis}")
